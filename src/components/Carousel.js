819bb24bdb24247f9b2786697e5a939459d78274
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-02-27 10:27:41 +0100

    prepare css file

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,9 @@
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 import useTranslations from '../hooks/useTranslations';
+import styles from '../../public/styles/Carousel.module.css'
 
+export default function Carousel() {
+
+
+}
