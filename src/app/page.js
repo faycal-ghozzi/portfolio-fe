a8d39eb744bbd133424cf4c3332aa62d34a82960
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-02-27 09:41:30 +0100

    initial setup

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Carousel from "../components/Carousel";
+
+export default function Home() {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+}
