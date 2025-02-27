a3a1ce23b2e5b304e97cd33a841f5256e95e647d
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-02-27 10:29:51 +0100

    preparing bilangual static text

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,6 +4,15 @@ import useTranslations from '../hooks/useTranslations';
 import styles from '../../public/styles/Carousel.module.css'
 
 export default function Carousel() {
+    const t = useTranslations();
 
+    const pages = [
+        { id: 1, content: t.welcome },
+        { id: 2, content: t.experience },
+        { id: 3, content: t.education },
+        { id: 4, content: t.certifications },
+        { id: 5, content: t.projects },
+        { id: 6, content: t.contact },
+    ];
 
 }
